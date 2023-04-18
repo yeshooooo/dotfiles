156925c36c937a7e457e3741a3b888d0f3dd0da1
yeshooo <81931672@qq.com>
2023-04-19 01:06:17 +0800

    修改polybar更适应双屏

--- polybar/.config/polybar/polybar-collection/launch.sh
+++ polybar/.config/polybar/polybar-collection/launch.sh
@@ -6,4 +6,19 @@ killall polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 CONFIG_DIR=$(dirname $0)/themes/$THEME/config.ini
-polybar main -c $CONFIG_DIR &
+# 原始
+#polybar main -c $CONFIG_DIR &
+
+#
+# https://github.com/polybar/polybar/issues/763
+#
+# 同时不要忘了在bar中设置monitor参数
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload  main -c $CONFIG_DIR &
+  done
+else
+
+  polybar --reload main -c $CONFIG_DIR &
+fi
