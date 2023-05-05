55df42217fef2ceb8a6367085633c402144e182b
yeshooo <81931672@qq.com>
2023-05-05 17:56:39 +0800

    添加ranger配置以及修改picom中的设置取消对jetbrains系列先前的限制透明

--- bspwm/.config/bspwm/scripts/screen-order.sh
+++ bspwm/.config/bspwm/scripts/screen-order.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-0 --off --output DP-0 --off --output DP-1 --off --output DP-2 --primary --mode 3840x2160 --pos 3440x0 --rotate normal --output DP-3 --off --output DP-4 --mode 3440x1440 --pos 0x250 --rotate normal --output DP-5 --off
+xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP-4 --off --output eDP-1-1 --mode 2560x1600 --pos 3840x1427 --rotate normal
