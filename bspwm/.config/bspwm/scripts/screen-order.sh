104acc221924e3577b42dbda15faca1ad5ff14b6
yeshooo <81931672@qq.com>
2023-07-29 23:29:50 +0800

    同步设置跟笔记本一样

--- bspwm/.config/bspwm/scripts/screen-order.sh
+++ bspwm/.config/bspwm/scripts/screen-order.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-0 --off --output DP-0 --off --output DP-1 --off --output DP-2 --primary --mode 3840x2160 --pos 3440x0 --rotate normal --output DP-3 --off --output DP-4 --mode 3440x1440 --pos 0x250 --rotate normal --output DP-5 --off
+xrandr --output HDMI-0 --off --output DP-0 --off --output DP-1 --off --output DP-2 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-3 --off --output DP-4 --mode 3840x2160 --pos 3840x0 --rotate normal --output DP-5 --off
