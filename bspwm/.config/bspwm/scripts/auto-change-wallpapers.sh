a1bf569e414b08d47ba6da6fd816d686011b5dec
yeshooo <81931672@qq.com>
2024-10-21 11:27:07 +0800

    自动切换壁纸在遇到文件夹的时候自动跳过

--- bspwm/.config/bspwm/scripts/auto-change-wallpapers.sh
+++ bspwm/.config/bspwm/scripts/auto-change-wallpapers.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
 while true; do
-	feh --bg-fill --randomize $HOME/Pictures/wallpapers/*
-	sleep 2m
+    # 遇到文件夹的时候跳过文件夹
+    wallpaper=$(find "$HOME/Pictures/wallpapers" -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" \) | shuf -n 1)
+    if [ -n "$wallpaper" ]; then
+        feh --bg-fill "$wallpaper"
+    fi
+    sleep 2m
 done
