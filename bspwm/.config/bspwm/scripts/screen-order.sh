6e7930bad228f005de415e757f336ec956c2a2f2
yeshooo <81931672@qq.com>
2024-10-22 10:48:44 +0800

    延迟启动vloctl,将音频所在显示器设置为HDMI,因DP不识别音频

--- bspwm/.config/bspwm/scripts/screen-order.sh
+++ bspwm/.config/bspwm/scripts/screen-order.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --primary --mode 3840x2160 --pos 2560x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off --output DP-6 --off --output DP-7 --off
+xrandr --output HDMI-1 --primary --mode 3840x2160 --pos 2560x0 --rotate normal --output DP-1 --mode 2560x1440 --pos 0x312 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off --output DP-6 --off --output DP-7 --off
